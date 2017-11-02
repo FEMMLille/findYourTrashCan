a1be1ac5e2e9f768fb09331a60afe65a5fdd43b3
Mathieu Saab <mathieu.saab@gmail.com>
2017-11-02 20:32:55 +0100

    <feat>Inscription Mise en place du système d'inscription, sans la connexion

--- findyourtrashcan-mobile/src/pages/signup/signup.ts
+++ findyourtrashcan-mobile/src/pages/signup/signup.ts
@@ -1,3 +1,7 @@
+import { WelcomePage } from '../pages';
+import { AccountDetailsService } from './../../providers/user/account-details';
+import { Credentials } from './../../shared/model/credentials';
+import { AccountDetails } from './../../shared/model/account-details';
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { IonicPage, NavController, ToastController } from 'ionic-angular';
@@ -14,40 +18,53 @@ export class SignupPage {
   // The account fields for the login form.
   // If you're using the username field with or without email, make
   // sure to add it to the type
-  account: { name: string, email: string, password: string } = {
-    name: 'Test Human',
-    email: 'test@example.com',
-    password: 'test'
-  };
+  account = new AccountDetails();
+  password: string;
+  cpassword: string;
 
   // Our translated text strings
   private signupErrorString: string;
+  private unmatchingPasswordsErrorString: string;
 
   constructor(public navCtrl: NavController,
     public user: User,
     public toastCtrl: ToastController,
-    public translateService: TranslateService) {
+    public translateService: TranslateService,
+    public accountDetails: AccountDetailsService) {
 
     this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
       this.signupErrorString = value;
+    })
+    this.translateService.get('UNMATCHING_PASSWORD_ERRORS').subscribe((value) => {
+      this.unmatchingPasswordsErrorString = value;
     })
   }
 
   doSignup() {
     // Attempt to login in through our User service
-    this.user.signup(this.account).subscribe((resp) => {
-      this.navCtrl.push(MainPage);
-    }, (err) => {
+    if (this.password == this.cpassword) {
+      this.accountDetails.signup(this.account).subscribe((resp) => {
 
-      this.navCtrl.push(MainPage);
-
-      // Unable to sign up
-      let toast = this.toastCtrl.create({
-        message: this.signupErrorString,
-        duration: 3000,
-        position: 'top'
+        this.navCtrl.push(MainPage);
+      }, (err) => {
+        this.showToastError(this.signupErrorString);
       });
-      toast.present();
+    } else {
+      this.showToastError(this.unmatchingPasswordsErrorString);
+    }
+  }
+
+  cancel() {
+    this.navCtrl.pop();
+  }
+
+  showToastError(message: string) {
+    // Unable to sign up
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'top'
     });
+    toast.present();
   }
 }
