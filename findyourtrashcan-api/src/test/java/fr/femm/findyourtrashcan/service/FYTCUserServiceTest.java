a33d94323b06e0b3b2b24e926dd73e209f8debf3
FCornaire <fc.cornaire@etudiant.univ-lille1.fr>
2017-11-04 00:20:45 +0100

    <feat> Inscription [Test] -Ajout des tests de création d'utilisateur et get d'un utilisateur -MAJ POM pour les dépendances de test

---
+++ findyourtrashcan-api/src/test/java/fr/femm/findyourtrashcan/service/FYTCUserServiceTest.java
@@ -0,0 +1,65 @@
+package fr.femm.findyourtrashcan.service;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fr.femm.findyourtrashcan.data.FYTCUser;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FYTCUserServiceTest {
+	
+	@Autowired
+	private FYTCUserService userService;
+	
+	@Test
+	public void createUserTest() {
+		//init
+		FYTCUser userToTest = new FYTCUser("maws", "bg", "wo@gmail.com");
+		
+		//execution
+		FYTCUser userCreated = userService.createUser(userToTest);
+		
+		//test  
+        assertNotNull(userCreated);
+        assertEquals(userCreated.getId(), userToTest.getId());
+        assertEquals(userCreated.getUsername(), userToTest.getUsername());
+        assertEquals(userCreated.getEmail(), userToTest.getEmail());
+	}
+	
+	@Test
+	public void getUserTest() {
+		//init
+		FYTCUser userToTest = new FYTCUser("maws", "bg", "wo@gmail.com");
+		
+		//execution
+		FYTCUser userCreated = userService.createUser(userToTest);
+		FYTCUser userGeted = userService.getUser(userCreated.getId());
+		
+		//test  
+        assertNotNull(userGeted);
+        assertEquals(userGeted.getId(), userToTest.getId());
+	}
+	
+	@Test
+	public void getUserTestByUsername() {
+		//init
+		FYTCUser userToTest = new FYTCUser("mat", "skra", "popopo@gmail.com");
+		
+		//execution
+		userService.createUser(userToTest);
+		FYTCUser userGeted = userService.getUser("mat");
+		
+		//test  
+        assertNotNull(userGeted);
+        assertEquals(userGeted.getUsername(), "mat");
+        assertEquals(userGeted.getUsername(), userToTest.getUsername());
+	}
+	
+}
