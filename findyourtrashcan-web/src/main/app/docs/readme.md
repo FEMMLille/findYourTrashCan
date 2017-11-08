169e1d15bdfbc8d2f5cf8dd7d7de693eee179db3
erwan <e.delbrouque-prial@etudiant.univ-lille1.fr>
2017-11-08 11:34:48 +0100

    ajout de l'architecture front du site vitrine et implémentation du composant de connexion et celui d'inscription en cours.

---
+++ findyourtrashcan-web/src/main/app/docs/readme.md
@@ -0,0 +1,9 @@
+# FindYourTrashcan
+
+Welcome to the project documentation!
+
+Use `npm run docs` for easier navigation.
+
+## Available documentation
+
+[[index]]
---
+++ findyourtrashcan-web/src/main/app/src/app/home/display/display.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '../../shared/shared.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    TranslateModule,
+    SharedModule
+  ],
+  declarations: [
+  ],
+  providers: [ ]
+})
+export class DisplayModule { }
