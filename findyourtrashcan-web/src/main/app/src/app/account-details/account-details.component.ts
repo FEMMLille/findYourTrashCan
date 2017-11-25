72b651e36e30459cf36f3b88f9d78e6fa6f043db
Mustapha Nezzari <mnezzari@gmail.com>
2017-11-25 13:36:38 +0100

    [USER_U_003] - Détails du compte + <fix> Ajout d'un profil test
    - Création de la page de profil Web 
    - Ajout d'un profil test pour que les test s'executent dans une base h2
    
    Github #14

--- findyourtrashcan-web/src/main/app/src/app/account-details/account-details.component.ts
+++ findyourtrashcan-web/src/main/app/src/app/account-details/account-details.component.ts
@@ -14,127 +14,24 @@ export class FormUser {
 }
 
 @Component({
-  selector: 'fytc-account-details',
+  selector: 'app-fytc-account-details',
   templateUrl: './account-details.component.html',
   styleUrls: ['./account-details.component.scss'],
 })
 
 export class AccountDetailsComponent implements OnInit {
 
-  createForm: FormGroup;
+  tiles = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 
-  form: FormUser;
-
-  constructor(private builder: FormBuilder) { }
+  constructor() { }
 
   ngOnInit() {
-    this.form = new FormUser();
-    this.createForm = new FormGroup({
-      'login': new FormControl(this.form.login, [
-        Validators.required,
-        Validators.minLength(4)
-      ]),
-      'password': new FormControl(this.form.password, [
-        Validators.required,
-        Validators.minLength(8)
-      ]),
-      'repeatPassword': new FormControl(this.form.repeatPassword, [
-        Validators.required,
-        Validators.minLength(8)
-      ]),
-      'email': new FormControl(this.form.email, [
-        Validators.required,
-        Validators.email
-      ]),
-      'firstName': new FormControl(this.form.firstName, [
-        Validators.required,
-        Validators.minLength(2)
-      ]),
-      'lastName': new FormControl(this.form.lastName, [
-        Validators.required,
-        Validators.minLength(2)
-      ]),
-      'birthday': new FormControl(this.form.birthday, [
-        Validators.required
-      ]),
-      'gender': new FormControl(this.form.gender, [
-        Validators.required
-      ])
-    });
+
   }
-
-  get login() {
-    return this.createForm.get('login');
-  }
-
-  getErrorMessageLogin() {
-    return this.login.hasError('required') ? 'You must enter a value' :
-      this.login.hasError('login') ? 'Not a valid login' : '';
-  }
-
-  get password() {
-    return this.createForm.get('password');
-  }
-
-  getErrorMessagePassword() {
-    return this.password.hasError('required') ? 'You must enter a value' :
-      this.password.hasError('password') ? 'Not a valid password' : '';
-  }
-
-  get repeatPassword() {
-    return this.createForm.get('repeatPassword');
-  }
-
-  getErrorMessageRepeatPassword() {
-    return this.repeatPassword.hasError('required') ? 'You must enter a value' :
-      this.repeatPassword.hasError('repeatPassword') ? 'Not a valid repeat password' : '';
-  }
-
-  get email() {
-    return this.createForm.get('email');
-  }
-
-  getErrorMessageMail() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-      this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  get firstName() {
-    return this.createForm.get('firstName');
-  }
-
-  getErrorMessageFirstName() {
-    return this.firstName.hasError('required') ? 'You must enter a value' :
-      this.firstName.hasError('firstName') ? 'Not a valid first name' : '';
-  }
-
-  get lastName() {
-    return this.createForm.get('lastName');
-  }
-
-  getErrorMessageLastName() {
-    return this.lastName.hasError('required') ? 'You must enter a value' :
-      this.lastName.hasError('lastName') ? 'Not a valid last name' : '';
-  }
-
-  get birthday() {
-    return this.createForm.get('birthday');
-  }
-
-  getErrorMessageBirthday() {
-    return this.birthday.hasError('required') ? 'You must enter a value' :
-      this.birthday.hasError('birthday') ? 'Not a valid birthday' : '';
-  }
-
-  get gender() {
-    return this.createForm.get('gender');
-  }
-
-  getErrorMessageGender() {
-    return this.gender.hasError('required') ? 'You must enter a value' :
-      this.gender.hasError('gender') ? 'Not a valid gender' : '';
-  }
-
-
 
 }
